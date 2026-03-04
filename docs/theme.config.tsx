e66cd25ff4466e941569fd49647a2fb2666390e1
abdssamie <abdessamie.elmoubarki@gmail.com>
2026-03-04 20:55:22 +0000

    docs: scaffold Nextra app

---
+++ docs/theme.config.tsx
@@ -0,0 +1,16 @@
+import type { DocsThemeConfig } from "nextra-theme-docs";
+
+const config: DocsThemeConfig = {
+  project: {
+    link: "https://example.com",
+  },
+  chat: {
+    link: "https://example.com",
+  },
+  docsRepositoryBase: "https://example.com",
+  footer: {
+    text: "Docs",
+  },
+};
+
+export default config;
