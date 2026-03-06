22e1d6a922f26b651bd142f5bbc0e4e8d3e6f573
abdssamie <abdessamie.elmoubarki@gmail.com>
2026-03-06 00:55:13 +0000

    chore: Configure ESLint to ignore build artifacts and add Convex generated files to .gitignore.

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,15 @@
 import nextConfig from "eslint-config-next";
+import { defineConfig } from "eslint/config";
 
-const eslintConfig = [
+const eslintConfig = defineConfig([
   ...nextConfig,
-];
+  {
+    ignores: [
+      ".next/**",
+      ".node_modules/**",
+      "docs/**"
+    ]
+  }
+]);
 
 export default eslintConfig;
